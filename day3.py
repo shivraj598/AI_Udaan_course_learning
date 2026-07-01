3d3c3b21817ff37082a1c0170b2fd25ec3f562ba
shivrajtimilsena <shivrajtimilsena@shivrajtimilsenas-MacBook-Air.local>
2026-07-01 20:46:11 +0545

    day3 learnign datatypes

---
+++ day3.py
@@ -0,0 +1,44 @@
+name ="manish"
+name2 = "anish"
+name3 = "nish"
+name4 = "ish"
+
+names = ["manish", "anish", "nish", "ish"] #list datatypes
+
+country1 = "japan"
+country2 = "usa"
+country3 = "nepal"
+
+countries = ["japan", "usa", "nepal"] #list
+# countries.append("india") #add india to last
+# countries.insert(1, "bangladesh") #add bangladesh to first index
+# countries.pop() #remove last element
+# countries.remove("usa") #remove usa
+# countries.sort() #sort in ascending order
+
+
+# print(len(countries))    #length of list
+# print(countries)
+
+
+countries_tuple = ("japan", "usa", "nepal") #tuple
+
+# print(countries_tuple.count("japan"))
+
+
+
+
+
+
+
+
+countries_set = {"japan", "usa", "nepal"} #set
+
+print(countries_set)
+
+
+# print(countries[0])
+
+
+
+
