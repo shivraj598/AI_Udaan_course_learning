d7dfc5d1456485dabb683b56a5e9510718c2215f
shivraj598 <timilsenashivraj598@gmail.com>
2026-07-02 20:24:15 +0545

    day4 learnign datatypes

---
+++ day4.py
@@ -0,0 +1,14 @@
+#      statement/block of code to be executed
+
+# age = 10
+# if age > 18:
+#      print("you can vote")
+# else:
+#      print("You cannot vote")
+
+password = "nepal12366"
+
+if password=="nepal123":
+     print("login success, password matched")
+else:
+     print("Login failed, password didnt matched")
